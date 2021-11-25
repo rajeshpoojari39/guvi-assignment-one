eb5b186508200d4dc3ea257adffebd7b06b5ae04
Rajesh Poojari <54307904+rajeshpoojari39@users.noreply.github.com>
2021-11-25 21:31:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var request = new XMLHttpRequest();
+request.open("GET", "https://jsonplaceholder.typicode.com/posts");
+request.send();
+request.onload = function () {
+  var data = JSON.parse(this.response);
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].id);
+  }
+};
